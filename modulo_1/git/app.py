52041019faf2d892c00b335e14813bf72d2c6e47
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-03 21:20:22 -0500

    programador bacon practica

--- modulo_1/git/app.py
+++ modulo_1/git/app.py
@@ -2,4 +2,13 @@ print("Bienvenido al semestre 3")
 print("Programacion III")
 
 
+<<<<<<< Updated upstream
 print("hola desde la ute")
+=======
+print("hola desde la ute")
+
+
+print("Yo soy un superprogramador")
+
+print("CAMBIO PARA GUARDAR EN EL STASH")
+>>>>>>> Stashed changes
